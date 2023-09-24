4297fe21de681b88816ecf9c71347f1b90814611
Vanessa Mercado <vanessa.mercado24@gmail.com>
2023-09-24 15:12:51 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1>GitFit</h1>
+      <h2>Sure, committing is cool, but how active are you?</h2>
+    </>
+  );
+}
+
+export default App;
